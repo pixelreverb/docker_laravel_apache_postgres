3c69b5b1143763f82e40ac0108bf8757717eeaf0
pixelreverb <146724815+pixelreverb@users.noreply.github.com>
2023-10-08 12:04:32 +0200

    Extend refresh lookup

--- src/vite.config.js
+++ src/vite.config.js
@@ -10,8 +10,14 @@ export default defineConfig({
     },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js'
+            ],
+            refresh: [
+                'routes/**',
+                'resources/**',
+            ],
         }),
     ],
 });
